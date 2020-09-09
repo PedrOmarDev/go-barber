9654bb1e976eb4b65792f2336bc1911ffa9537e0
Pedro Omar <pedromarneto@hotmail.com>
2020-09-08 22:55:38 -0300

    Adicionado validação de datas e criado a primeira model da aplicação, a model de Appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,29 @@
 import { Router } from 'express'
-import { uuid } from 'uuidv4'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+import Appointment from '../models/Appointment'
 
 const appointmentsRouter = Router()
 
-const appointments = []
+const appointments: Appointment[] = []
+
+appointmentsRouter.get('/', (request, response) => {
+	return response.json()
+})
 
 appointmentsRouter.post('/', (request, response) => {
 	const { provider, date } = request.body
 
-	const appointment = {
-		id: uuid(),
-		provider,
-		date,
-	}
+	const parsedDate = startOfHour(parseISO(date))
+	const findAppointmentInSameDate = appointments.find(appointment =>
+		isEqual(appointment.date, parsedDate),
+	)
+
+	if (findAppointmentInSameDate)
+		return response
+			.status(400)
+			.json({ message: 'This appointment is already booked' })
+
+	const appointment = new Appointment(provider, parsedDate)
 
 	appointments.push(appointment)
 
