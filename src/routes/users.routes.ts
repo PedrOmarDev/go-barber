e2f19ae227bf55b2c882287b10075e630e73f4f8
Pedro Omar <pedromarneto@hotmail.com>
2020-09-28 22:11:30 -0300

    Adicionado funcionalidade de atualizar avatar do usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
+import multer from 'multer'
+
+import uploadConfig from '../config/upload'
 import UserMap from '../mappers/UserMap'
 
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
 	try {
@@ -24,5 +31,27 @@ usersRouter.post('/', async (request, response) => {
 		return response.status(400).json({ error: err.message })
 	}
 })
+
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('avatar'),
+	async (request, response) => {
+		try {
+			const updateUserAvatar = new UpdateUserAvatarService()
+
+			const user = await updateUserAvatar.execute({
+				user_id: request.user.id,
+				avatar_filename: request.file.filename,
+			})
+
+			const mappedUser = UserMap.toDTO(user)
+
+			return response.json(mappedUser)
+		} catch (err) {
+			return response.status(400).json({ error: err.message })
+		}
+	},
+)
 
 export default usersRouter
