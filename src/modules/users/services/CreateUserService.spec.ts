5c6fecda6502c3ba17ba73cb7ee085bc2212d4da
Pedro Omar <pedromarneto@hotmail.com>
2020-11-11 07:47:09 -0300

    Finalizando os testes dos services já criados

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,48 @@
+import AppError from '@shared/errors/AppError'
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+import CreateUserService from './CreateUserService'
+
+describe('CreateUser', () => {
+	it('should be able to create new user', async () => {
+		const fakeUsersRepository = new FakeUsersRepository()
+		const fakeHashProvider = new FakeHashProvider()
+
+		const createUser = new CreateUserService(
+			fakeUsersRepository,
+			fakeHashProvider,
+		)
+
+		const user = await createUser.execute({
+			email: 'pedro.omar@introduce.com.br',
+			name: 'Pedro Omar',
+			password: '123456',
+		})
+
+		expect(user).toHaveProperty('id')
+	})
+
+	it('should not be able to create new user with an email already used', async () => {
+		const fakeUsersRepository = new FakeUsersRepository()
+		const fakeHashProvider = new FakeHashProvider()
+
+		const createUser = new CreateUserService(
+			fakeUsersRepository,
+			fakeHashProvider,
+		)
+
+		await createUser.execute({
+			email: 'pedro.omar@introduce.com.br',
+			name: 'Pedro Omar',
+			password: '123456',
+		})
+
+		expect(
+			createUser.execute({
+				email: 'pedro.omar@introduce.com.br',
+				name: 'Pedro Omar',
+				password: '123456',
+			}),
+		).rejects.toBeInstanceOf(AppError)
+	})
+})
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,12 +1,10 @@
-import path from 'path'
-import fs from 'fs'
 import { inject, injectable } from 'tsyringe'
 
 import AppError from '@shared/errors/AppError'
-import uploadConfig from '@config/upload'
 
 import User from '../infra/typeorm/entities/User'
 import IUsersRepository from '../repositories/IUsersRepository'
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 
 interface IRequest {
 	user_id: string
@@ -18,6 +16,9 @@ class UpdateUserAvatarService {
 	constructor(
 		@inject('UsersRepository')
 		private usersRepository: IUsersRepository,
+
+		@inject('StorageProvider')
+		private storageProvider: IStorageProvider,
 	) {}
 
 	public async execute({
@@ -34,21 +35,12 @@ class UpdateUserAvatarService {
 		}
 
 		if (user.avatar) {
-			const userAvatarFilePath = path.join(
-				uploadConfig.directory,
-				user.avatar,
-			)
-
-			const userAvatarFileExists = await fs.promises.stat(
-				userAvatarFilePath,
-			)
-
-			if (userAvatarFileExists) {
-				await fs.promises.unlink(userAvatarFilePath)
-			}
+			await this.storageProvider.deleteFile(user.avatar)
 		}
 
-		user.avatar = avatar_filename
+		const filename = await this.storageProvider.saveFile(avatar_filename)
+
+		user.avatar = filename
 
 		await this.usersRepository.update(user)
 
