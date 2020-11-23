e27d0f283ba82338e0f674e393211896bacbff82
Pedro Omar <pedromarneto@hotmail.com>
2020-11-23 06:33:07 -0300

    Refatoração nos testes, adicionando no beforeEach de cada teste o que era usado em todos os testes daquele arquivo, para compartilhar as variáveis e deixar o código mais legível

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -3,11 +3,16 @@ import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
 import CreateUserService from './CreateUserService'
 
-describe('CreateUser', () => {
-	it('should be able to create new user', async () => {
-		const fakeUsersRepository = new FakeUsersRepository()
-		const fakeHashProvider = new FakeHashProvider()
+let fakeUsersRepository: FakeUsersRepository
+let fakeHashProvider: FakeHashProvider
 
+describe('CreateUser', () => {
+	beforeEach(() => {
+		fakeUsersRepository = new FakeUsersRepository()
+		fakeHashProvider = new FakeHashProvider()
+	})
+
+	it('should be able to create new user', async () => {
 		const createUser = new CreateUserService(
 			fakeUsersRepository,
 			fakeHashProvider,
@@ -23,9 +28,6 @@ describe('CreateUser', () => {
 	})
 
 	it('should not be able to create new user with an email already used', async () => {
-		const fakeUsersRepository = new FakeUsersRepository()
-		const fakeHashProvider = new FakeHashProvider()
-
 		const createUser = new CreateUserService(
 			fakeUsersRepository,
 			fakeHashProvider,
