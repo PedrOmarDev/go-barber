66b206f83dd1d26c239b89722a72267a7b52a03e
Pedro Omar <pedromarneto@hotmail.com>
2020-12-07 07:54:16 -0300

    Implementando mais alguns testes no módulo de appointments

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,59 @@
-import { getRepository, Repository } from 'typeorm'
-
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import { getRepository, Raw, Repository } from 'typeorm'
 
 import Appointment from '../entities/Appointment'
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+import IFindAllInMonthProviderDTO from '@modules/appointments/dtos/IFindAllInMonthProviderDTO'
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO'
 
 class AppointmentsRepository implements IAppointmentsRepository {
 	private ormRepository: Repository<Appointment>
 
 	constructor() {
 		this.ormRepository = getRepository(Appointment)
+	}
+
+	public async findAllInMonthFromProvider({
+		provider_id,
+		month,
+		year,
+	}: IFindAllInMonthProviderDTO): Promise<Appointment[]> {
+		const parsedMonth = String(month).padStart(2, '0')
+
+		const appointments = await this.ormRepository.find({
+			where: {
+				provider_id,
+				date: Raw(
+					dateFieldName =>
+						`to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+				),
+			},
+		})
+
+		return appointments
+	}
+
+	public async findAllInDayFromProvider({
+		provider_id,
+		day,
+		month,
+		year,
+	}: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+		const parsedDay = String(day).padStart(2, '0')
+		const parsedMonth = String(month).padStart(2, '0')
+
+		const appointments = await this.ormRepository.find({
+			where: {
+				provider_id,
+				date: Raw(
+					dateFieldName =>
+						`to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+				),
+			},
+		})
+
+		return appointments
 	}
 
 	public async findByDate(date: Date): Promise<Appointment | undefined> {
--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,13 +1,47 @@
 import { uuid } from 'uuidv4'
-import { isEqual } from 'date-fns'
-
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
-import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+import { getDate, getMonth, getYear, isEqual } from 'date-fns'
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+import IFindAllInMonthProviderDTO from '@modules/appointments/dtos/IFindAllInMonthProviderDTO'
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO'
 
 class AppointmentsRepository implements IAppointmentsRepository {
 	private appointments: Appointment[] = []
+
+	public async findAllInMonthFromProvider({
+		provider_id,
+		month,
+		year,
+	}: IFindAllInMonthProviderDTO): Promise<Appointment[]> {
+		const appointments = this.appointments.filter(
+			appointment =>
+				appointment.provider_id === provider_id &&
+				getMonth(appointment.date) + 1 === month &&
+				getYear(appointment.date) === year,
+		)
+
+		return appointments
+	}
+
+	public async findAllInDayFromProvider({
+		provider_id,
+		day,
+		month,
+		year,
+	}: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+		const appointments = this.appointments.filter(
+			appointment =>
+				appointment.provider_id === provider_id &&
+				getDate(appointment.date) === day &&
+				getMonth(appointment.date) + 1 === month &&
+				getYear(appointment.date) === year,
+		)
+
+		return appointments
+	}
 
 	public async findByDate(date: Date): Promise<Appointment | undefined> {
 		const findAppointmentInSameDate = this.appointments.find(appointment =>
