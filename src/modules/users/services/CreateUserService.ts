aec21c13ac5266433f969676c4727e08b073a72b
Pedro Omar <pedromarneto@hotmail.com>
2021-01-20 15:54:15 -0300

    Testando animação do github

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -29,6 +29,7 @@ class CreateUserService {
 			throw new AppError('Email adress already used.')
 		}
 
+		// Testando animação do Github
 		const hashedPassword = await this.hashProvider.generateHash(password)
 
 		const user = await this.usersRepository.create({
